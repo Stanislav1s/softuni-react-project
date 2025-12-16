4cea5ab1c5a7cc8e52fd2d87a00b06b8da1ed606
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-12-16 08:22:23 +0200

    Update Header to use authentication context

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import UserContext from "../../contexts/UserContext.jsx";
 
 export default function Header() {
+    const { isAuthenticated } = useContext(UserContext)
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3">
             <div className="container">
@@ -30,24 +33,32 @@ export default function Header() {
                                 Recipes
                             </Link>
                         </li>
+                        {!isAuthenticated
+                            ? (<>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login">
+                                        Login
+                                    </Link>
+                                </li>
 
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">
-                                Login
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">
-                                Register
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/logout">
-                                Logout
-                            </Link>
-                        </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/register">
+                                        Register
+                                    </Link>
+                                </li> </>) :
+                            (<>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/favourites">
+                                        Favourites
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/logout">
+                                        Logout
+                                    </Link>
+                                </li>
+                            </>)
+                        }
                     </ul>
                 </div>
             </div>
